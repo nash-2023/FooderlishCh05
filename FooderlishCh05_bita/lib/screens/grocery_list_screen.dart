ab9da60b62331113bc79517d0d6d6b268554d624
nash-2023 <abdalla.fat7y@hotmail.com>
2024-01-01 16:59:08 +0200

    265 ++

--- FooderlishCh05_bita/lib/screens/grocery_list_screen.dart
+++ FooderlishCh05_bita/lib/screens/grocery_list_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/grocery_tile.dart';
 import '../models/models.dart';
-import 'grocery_item_screen.dart';
 
 class GroceryListScreen extends StatelessWidget {
   final GroceryManager manager;
